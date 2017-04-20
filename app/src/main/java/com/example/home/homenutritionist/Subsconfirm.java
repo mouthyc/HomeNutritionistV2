139d498d8591b736540a47e06256000675a0cdd1
Jason <soduck102@gmail.com>
2017-04-21 00:32:54 +0800

    subscribeconfirm menu done

---
+++ app/src/main/java/com/example/home/homenutritionist/Subsconfirm.java
@@ -0,0 +1,30 @@
+package com.example.home.homenutritionist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class Subsconfirm extends AppCompatActivity {
+    private Button Confirmback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.subscribeconfirm);
+
+
+        Confirmback = (Button) findViewById(R.id.confirmbtn);
+
+        Confirmback.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(Subsconfirm.this, "Thank you for the subs! Our consultant will contact you soon.", Toast.LENGTH_SHORT).show();
+
+                startActivity(new Intent(Subsconfirm.this, HomeActivity.class));
+            }
+        });
+    }
+}
