34913a6b9f8ef3caa46fc76a9b511475fdd43226
Jason <soduck102@gmail.com>
2017-04-20 23:31:41 +0800

    menu updated

--- app/src/main/java/com/example/home/homenutritionist/MenuActivity.java
+++ app/src/main/java/com/example/home/homenutritionist/MenuActivity.java
@@ -1,7 +1,11 @@
 package com.example.home.homenutritionist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MenuActivity extends AppCompatActivity {
@@ -9,5 +13,9 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+    }
+
+    public void clickphoto(View v) {
+        startActivity(new Intent(MenuActivity.this, ContactUsActivity.class));
     }
 }
